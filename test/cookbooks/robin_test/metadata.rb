f5b3dfb6a4f42ba3392ac856393056cd326d4d2b
Benjamin Confino <benjamic@uk.ibm.com>
2014-09-22 19:17:46 +0100

    Update metadata.rb

--- test/cookbooks/robin_test/metadata.rb
+++ test/cookbooks/robin_test/metadata.rb
@@ -3,10 +3,6 @@ license          'Apache 2.0'
 description      'Test cookbook for ci.chef.ihs.config cookbook'
 
 depends "apt"
-depends "wlp"
-depends "ihs"
 depends "java"
 depends "wlp"
-depends "samples"
-depends "ci.chef.ihs.config"
- 
+depends "ihs"
